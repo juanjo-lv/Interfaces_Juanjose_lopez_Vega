67797fc5f144922286c19a71ba7a3bc8508a5217
juanjo-lv <60658705+juanjo-lv@users.noreply.github.com>
2021-02-03 17:19:25 +0100

    update

--- EjericiciosWebComponents/Ejercicio3/scripts.js
+++ EjericiciosWebComponents/Ejercicio3/scripts.js
@@ -1,15 +1,21 @@
 class elementDetails extends HTMLElement {
     constructor() {
         super();
-        
+
+
         this.temp = document.getElementById("miTemplate").content;
         this.clon = this.temp.cloneNode(true);
         
+
+        this.attachShadow({mode :'open'})
+        // se crea automaticamente una variable llamada shadowRoot 
+
         
+
         
     }
     connectedCallback(){
-        this.appendChild(this.clon);
+        this.shadowRoot.appendChild(this.clon);
     }
 }
 customElements.define("element-details", elementDetails);
