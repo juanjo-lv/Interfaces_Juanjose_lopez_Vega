fec4b326283fbfabb8fd8d3d5f91b7bb90d4ad6e
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-02-06 19:59:47 +0100

    practica 4 avance y ejercicio 8 web components

---
+++ EjericiciosWebComponents/Ejercicio8/scripts.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM cargado");
+
+});
+
+
+var myTemplate = document.createElement('template');
+myTemplate.innerHTML =`
+<style>
+#contenedor{
+    margin-top: 1rem;
+    border: 1px solid black;
+    width: 50vw;
+    height: 10vh;
+}
+#barra{
+    width: 0%;
+    height: 100%;
+    background-color: red;
+   
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;    
+}
+</style>
+    <button id="aceptar">Aceptar</button>
+    <button id="parar">Parar</button>
+    <div id="contenedor">
+        <div id="barra"></div>
+    </div>
+`
+class progressBar extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({ mode: "open" });
+        this.clon = myTemplate.content.cloneNode(true);
+    }
+    connectedCallback(){
+         
+         if(this.revisar()==false){
+                alert("datos incorrectos revisa el html")
+         }else{
+            this.shadowRoot.appendChild(this.clon);
+         }
+         
+         this.app();
+         
+      
+    }
+    revisar(){
+        const time = this.segundos || 0;
+        if(time == 0 || time<0){
+            return false;
+        }
+    }
+    app() {
+        var inter;
+        const time = this.segundos || 0;
+        const botonCrecer = this.shadowRoot.getElementById("aceptar");
+        const barra = this.shadowRoot.getElementById("barra");
+        var progreso = 0;
+        botonCrecer.addEventListener("click", () => {
+          inter = setInterval(() => {
+      
+            if (barra.style.width == `100%`) {
+              clearInterval(inter);
+            }else{
+              barra.style.width = `${progreso}%`;
+             
+             if(progreso<100){
+                 progreso++;
+                 console.log(progreso)
+             }
+              
+            }
+              barra.innerHTML = `<span>${progreso}%</span>`;
+          }, time / 100);
+        });
+        
+            const botonParar = this.shadowRoot.getElementById("parar");
+            botonParar.addEventListener("click",()=>{
+               clearInterval(inter);
+            })
+         
+      }
+
+      get segundos(){
+          return this.getAttribute("seconds")
+      } 
+}
+customElements.define("progress-bar", progressBar);
+
--- JuanJose_Lopez_Vega_Practica4/JuanJose_Lopez_Vega_Practica4.js
+++ JuanJose_Lopez_Vega_Practica4/JuanJose_Lopez_Vega_Practica4.js
@@ -1,3 +1,64 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    console.log("DOM cargado")
-})
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM cargado");
+//   tiempo();
+  mostrar();
+  //ocultar();
+});
+
+function tiempo() {
+  var elemento = document.getElementById("tiempo");
+  var contador = document.getElementById("contador")
+  
+  var hora = 0;
+  var min = 0;
+  var sec = 0;
+  
+elemento.addEventListener("click",()=>{
+     let temp = setInterval(() => {
+         sec++;
+         if(sec==59){
+             sec=0;
+             min++;
+             if(min==59){
+                sec =0;
+                min = 0
+                hora++;
+            } 
+         }
+         contador.innerHTML=`${hora}h ${min}min ${sec}s`
+     }, 1000);
+});  
+}
+function mostrar() {
+    let barra = document.querySelector(".reglas");
+    let oculto = document.querySelector(".desplegado")
+
+    barra.addEventListener("click",()=>{
+        oculto.style.transition = "1s";
+        oculto.style.height ="20rem" ;
+
+        var despe = setTimeout(() => {
+          oculto.style.opacity = "1";
+          clearTimeout(despe);
+        }, 500);
+
+    })
+    
+}
+function ocultar() {
+  let barra = document.querySelector(".reglas");
+  let oculto = document.querySelector(".desplegado")
+    
+      barra.addEventListener("click",()=>{
+        oculto.style.opacity = "0";
+        setTimeout(() => {
+          oculto.style.height ="0px" ;
+        }, 2000);
+       
+        
+        
+                
+    })
+}
+
+
