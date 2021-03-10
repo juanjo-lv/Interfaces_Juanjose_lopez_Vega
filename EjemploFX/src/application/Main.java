ff42baf99f0dd2c0605ccc4646c485fabab40c0c
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-03-10 12:48:21 +0100

    as

--- EjemploFX/src/application/Main.java
+++ EjemploFX/src/application/Main.java
@@ -1,18 +1,24 @@
 package application;
 	
+import javafx.animation.*;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
+
 public class Main extends Application {
-	@Override
+	private BorderPane rootLayout;
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("border.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			//MOstramos la escena del BorderPane de la variable rootLayout
+			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
