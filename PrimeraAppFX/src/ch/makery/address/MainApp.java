73d208af1380e1f0a6abb76c877857f2c88b81b0
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-03-11 11:50:03 +0100

    dd

--- PrimeraAppFX/src/ch/makery/address/MainApp.java
+++ PrimeraAppFX/src/ch/makery/address/MainApp.java
@@ -1,15 +1,55 @@
 package ch.makery.address;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class MainApp extends Application {
 
+public class MainApp extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApp");
 		
+		initRootLayout();
+		showPersonOverview();
 	}
-
+	public void initRootLayout() {
+		//Carga el XML con el diseño principal
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void showPersonOverview() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+			AnchorPane personOverview = (AnchorPane) loader.load();
+			rootLayout.setCenter(personOverview);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
--- PrimeraAppFX/src/module-info.java
+++
@@ -1,5 +0,0 @@
-module PrimeraAppFX {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
